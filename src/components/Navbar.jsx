80a2f9b727dfd57aca3b98da5067d74018c24d9d
23IT006 <23IT006@licet.com>
2026-03-05 12:43:37 +0530

    logo added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,34 @@
 import { Link } from "react-router-dom";
 import "../index.css";
 import licet from "../assets/Images/licet.jpg";
+import logo1 from "../assets/Images/eventlogo.png";   // event logo
+import logo2 from "../assets/Images/partnerlogo.png";   // partner logo
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
 
-        {/* Logo */}
-        <div className="logo">
-          <img src={licet} alt="Licet Logo" />
-          <span className="logo-text">LICET</span>
+        {/* CENTER SECTION */}
+        <div className="nav-center">
+
+          <div className="logo">
+            <img src={licet} alt="Licet Logo" />
+            <span className="logo-text">LICET</span>
+          </div>
+
+          <div className="nav-links">
+            <Link to="/">HOME</Link>
+            <Link to="/events">EVENTS</Link>
+            <Link to="/contact">CONTACT</Link>
+          </div>
+
         </div>
 
-        {/* Links */}
-        <div className="nav-links">
-          <Link to="/">HOME</Link>
-          <Link to="/events">EVENTS</Link>
-          <Link to="/contact">CONTACT</Link>
+        {/* RIGHT LOGOS */}
+        <div className="right-logos">
+          <img src={logo1} alt="Event Logo" />
+          <img src={logo2} alt="Partner Logo" />
         </div>
 
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import ParticleBackground from "../components/ParticleBackground";
 import StarField from "../components/StarField";
 import PageWrapper from "../components/PageWrapper";
 import bgImage from "../assets/Images/tech.jpg";
+import eventLogo from "../assets/Images/eventlogo.png";
+import partnerLogo from "../assets/Images/partnerlogo.png";
 import "../index.css";
 
 function Home() {
@@ -14,9 +16,23 @@ function Home() {
         <StarField />
         <ParticleBackground />
 
+        {/* Logos */}
+        <div className="logo-section">
+          <div className="logo-box">
+            <img src={eventLogo} alt="Event Logo" />
+            <p>TECHNOVA</p>
+          </div>
+
+          <div className="logo-box">
+            <img src={partnerLogo} alt="Partner Logo" />
+            <p>Organized by LICET IT</p>
+          </div>
+        </div>
+
         <div className="event-date">
           Be There. 17th March 2026.
         </div>
+
       </div>
     </PageWrapper>
   );
